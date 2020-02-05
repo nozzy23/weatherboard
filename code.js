37a0e63d56eda0fe08de67ba05a4163ae0e27842
Oscar Nunez <noscar07@gmail.com>
2020-02-05 18:28:30 -0500

    added enter key value in search bar

--- code.js
+++ code.js
@@ -10,10 +10,18 @@
  create a area to dump the city infomation.
  and broadcast the next 5 day forcast.
  */
-  var userCity= $(".cities").keyup(function() {
-    var value = $(this).val();
-    $("p").text(value);
-    }).keyup();
+  var userCity= $(".cities").keydown(function(e) {
+
+    if( e.key == "Enter" ) {
+      e.preventDefault();
+      var value = $(this).val();
+      console.log(value)
+      $("p").text(value);
+      //console.log(e)
+    }
+
+    
+  })
 
   var locationURL= "https://api.openweathermap.org/data/2.5/weather?q=miami,florida&units=imperial&appid=849cc81e3938b0e67ca2c053350dac29"
 
@@ -22,11 +30,19 @@ $.ajax({
   url: locationURL,
   method: "GET"
 }).then(function(response){
-console.log(response);
 $("#cityTitle").text("cities title:" + response.name);
 $("#cityTemperature").text("Temperature:" + response.main.temp);
 $("#cityHumidity").text("Humidity:" + response.main.humidity);
 $("#cityWindspeed").text("Windspeed:" + response.wind.speed);
 });
 
+var UvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid=849cc81e3938b0e67ca2c053350dac29&lat=37.75&lon=122.37&cnt={1}"
+
+$.ajax({
+  url: UvIndexURL,
+  method: "GET"
+}).then(function(response){
+console.log(response);
+$("#UVindex").text("UV?: " + response.value);
+});
 
