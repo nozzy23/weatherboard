d76f0e28d76a50af46e78c6fa75684cd136f6d4b
Oscar Nunez <noscar07@gmail.com>
2020-02-05 15:14:09 -0500

    accessing information from third party API

--- code.js
+++ code.js
@@ -10,3 +10,23 @@
  create a area to dump the city infomation.
  and broadcast the next 5 day forcast.
  */
+  var userCity= $(".cities").keyup(function() {
+    var value = $(this).val();
+    $("p").text(value);
+    }).keyup();
+
+  var locationURL= "https://api.openweathermap.org/data/2.5/weather?q=miami,florida&units=imperial&appid=849cc81e3938b0e67ca2c053350dac29"
+
+
+$.ajax({
+  url: locationURL,
+  method: "GET"
+}).then(function(response){
+console.log(response);
+$("#cityTitle").text("cities title:" + response.name);
+$("#cityTemperature").text("Temperature:" + response.main.temp);
+$("#cityHumidity").text("Humidity:" + response.main.humidity);
+$("#cityWindspeed").text("Windspeed:" + response.wind.speed);
+});
+
+
