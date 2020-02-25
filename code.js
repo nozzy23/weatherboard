f82545614adcc4b1ac027c9c08457eaf9274e6f2
Oscar Nunez <noscar07@gmail.com>
2020-02-24 23:40:43 -0500

    working on adding history and local storage

--- code.js
+++ code.js
@@ -15,13 +15,17 @@
  The lon and lat of the city is located in the object. So I need to get the input of the city get the lat and lon build that onto the new url string and display on screen.
  */
 
+
+var apiKey= "849cc81e3938b0e67ca2c053350dac29";
+
 //suppose to add to history
-  /*var userCity= $(".cities").keydown(function(e) {
-    if( e.key == "Enter" ) {
-      e.preventDefault();
-      var value = $(this).val();
-      console.log(value)
-      $(".searchHistory").text(value);
+ 
+var userCity= $("#toDestination").on("click",function(e) {
+  if( e.key == "Enter" ) {
+    e.preventDefault();
+    var value = $(this).val();
+    console.log(value)
+    $(".searchHistory").text(value);
     }
     var addList= $("<li><li>");
     addList.addClass("list-group-item");
@@ -29,58 +33,7 @@
     $(".searchHistory").append(addList);
   });
 
-  var UvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid=849cc81e3938b0e67ca2c053350dac29&lat=37.75&lon=122.37&cnt={1}"
 
-$.ajax({
-  url: UvIndexURL,
-  method: "GET"
-}).then(function(response){
-$("#UVindex").text("UV?: " + response.value);
-});
-
-var fiveDayURL= "http://api.openweathermap.org//data/2.5/forecast?q=miami,florida&mode=xml&appid=849cc81e3938b0e67ca2c053350dac29units=imperial"
-
-$.ajax({
-  url: fiveDayURL,
-  method: "GET"
-}).then(function(response){
-console.log(response);
-var fiveday=$("<img>");
-
-$("#fiveDayCast").text(response.forecast);
-});*/
-
-/*function getUV(){
-$.ajax({
-  url: UvIndexURL,
-  method: "GET"
-}).then(function(response){
-var cityLon=(response.coord.lon)
-var cityLat=(response.coord.lat)
-console.log(cityLon);
-console.log(cityLat);
-});
-}*/
-/*$(".btn-city").on("click," ,function(){
-  var UvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+ "849cc81e3938b0e67ca2c053350dac29&lat={lat}&lon={lon}";
-
-  $.ajax({
-    url: UvIndexURL,
-    method: "GET"
-  }).then(function(response){
-  var cityLon=(response.coord.lon)
-  var cityLat=(response.coord.lat)
-  var UvIndexURL= "http://api.openweathermap.org/data/2.5/uvi?appid=" +apiKey+ "&lat="+cityLat+ "&lon" + cityLon;
-  $("#UVindex").text("uv: " + UvIndexURL);*/
-
-
-var apiKey= "849cc81e3938b0e67ca2c053350dac29";
-
-var locationURL ="https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + apiKey;
-
-var UvIndexURL ="http://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey+"&lat={lat}&lon={lon}";
-
-var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q={city name},{state}&appid=" + apiKey;
 
 // completed. gets value and display city.
 $(".btn-city").on("click", function(event){
@@ -102,18 +55,8 @@ $(".btn-city").on("click", function(event){
 
 
 
-/*function getLon(lon){
-  var city=$(".form-control").val();
-  var locationURL= "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=" + apiKey;
-  $.ajax({
-    url: locationURL,
-    method: "GET"
-  }).then(function(response){
-  (response.coord.lon);
-  });
-};*/
 
-
+//function to get The Lat and Lon
 function getCoord(callback){
   var city=$(".form-control").val();
   var locationURL= "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=" + apiKey;
@@ -149,9 +92,30 @@ $(".btn-city").on("click", function(event){
 
 
 
-/*
+
 //five-day forecast
 $(".btn-city").on("click", function(event){
   event.preventDefault();
+  var city=$(".form-control").val();
+  var fiveDayUrl = "http://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=" + apiKey;
 
-})*/
+  $.ajax({
+    url: fiveDayUrl,
+    method: "GET"
+  }).then(function(response){
+    var fiveDayIcon = (response.weatherdata.forecast)
+    var iconSpot = $("<img>").attr("src", fiveDayIcon);
+    $("#fiveDayCast").append(iconSpot);
+  })
+
+})
+
+//Local storage 
+function sayCity(){
+  console.log($("#firstCity").val())
+  localStorage.setItem("cityOne",$(".list-group-item").val());
+}
+
+function getCity(){
+  $(".list-group-item").val(localStorage.getItem("cityOne"));
+}
