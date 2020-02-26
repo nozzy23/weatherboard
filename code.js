0eed578677e25037cfa59f30a697a2e0f60edf92
Oscar Nunez <noscar07@gmail.com>
2020-02-26 13:27:52 -0500

    final push

--- code.js
+++ code.js
@@ -20,14 +20,13 @@ var apiKey= "849cc81e3938b0e67ca2c053350dac29";
 
 //suppose to add to history
  
-var userCity= $("#toDestination").on("click",function(e) {
+var userCity= $(".btn-city").on("click",function(e) {
   if( e.key == "Enter" ) {
     e.preventDefault();
-    var value = $(this).val();
-    console.log(value)
+    var value = $(".form-control").val();
     $(".searchHistory").text(value);
     }
-    var addList= $("<li><li>");
+    var addList= $("<li>");
     addList.addClass("list-group-item");
     addList.text(value);
     $(".searchHistory").append(addList);
@@ -100,22 +99,29 @@ $(".btn-city").on("click", function(event){
     url: fiveDayUrl,
     method: "GET"
   }).then(function(response){
-    console.log("5days", response);
+    console.log("5days", response.list[0].weather[0]);
+    var icons= response.list[0].weather[0].id;
+    var IconsLocatation= $("<img>").attr("src", icons)
+    $(".tempIconOne").append(IconsLocatation);
     $(".card-titleOne").text(response.list[0].dt_txt);
-    $(".card-temp").text("Temp " + response.list[0].main.temp)
-    $(".card-humidity").text("Humid: " +response.list[0].main.humidity)
+    $(".card-temp").text("Temp " + response.list[0].main.temp);
+    $(".card-humidity").text("Humid: " +response.list[0].main.humidity);
+    $(".tempIconTwo").append(IconsLocatation);
     $(".card-titleTwo").text(response.list[1].dt_txt);
-    $(".card-tempTwo").text("Temp " + response.list[1].main.temp)
-    $(".card-humidityTwo").text("Humid: " +response.list[1].main.humidity)
+    $(".card-tempTwo").text("Temp " + response.list[1].main.temp);
+    $(".card-humidityTwo").text("Humid: " +response.list[1].main.humidity);
+    $(".tempIconThree").append(IconsLocatation);
     $(".card-titleThree").text(response.list[2].dt_txt);
-    $(".card-tempThree").text("Temp " + response.list[2].main.temp)
-    $(".card-humidityThree").text("Humid: " +response.list[2].main.humidity)
+    $(".card-tempThree").text("Temp " + response.list[2].main.temp);
+    $(".card-humidityThree").text("Humid: " +response.list[2].main.humidity);
+    $(".tempIconFour").append(IconsLocatation);
     $(".card-titleFour").text(response.list[3].dt_txt);
-    $(".card-tempFour").text("Temp " + response.list[3].main.temp)
-    $(".card-humidityFour").text("Humid: " + response.list[3].main.humidity)
+    $(".card-tempFour").text("Temp " + response.list[3].main.temp);
+    $(".card-humidityFour").text("Humid: " + response.list[3].main.humidity);
+    $(".tempIconFive").append(IconsLocatation);
     $(".card-titleFive").text(response.list[4].dt_txt);
-    $(".card-tempFive").text("Temp " + response.list[4].main.temp)
-    $(".card-humidityFive").text("Humid: " +response.list[4].main.humidity)
+    $(".card-tempFive").text("Temp " + response.list[4].main.temp);
+    $(".card-humidityFive").text("Humid: " +response.list[4].main.humidity);
   })
 
 })
@@ -145,7 +151,7 @@ $(document).ready(function(){
 
 //Local storage  
 function sayCity(){
-  localStorage.setItem("cityOne",$(".list-group-item").val());
+  localStorage.setItem("cityOne",$(".form-control").val());
 }
 
 function getCity(){
